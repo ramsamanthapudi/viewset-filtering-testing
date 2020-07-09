98f930559b6e6ead69ad1a10518c01ce7af1ebf7
ramsamanthapudi <ramsamanthapudi@gmail.com>
2020-07-10 02:40:41 +0530

    viewset other http methods added by adding mixins

--- vsproject/vsapp/urls.py
+++ vsproject/vsapp/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path, include
 from .models import Student
 from .views import viewsetclass
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()  # creating router object and adding our view class to it
+router.register(r"student", viewsetclass)
 
 urlpatterns = [
-    path('student/', viewsetclass.as_view({'get':'list'})),
+    #path('student/', viewsetclass.as_view({'get':'list'})),
+    path('', include(router.urls))
     ]
--- vsproject/vsapp/views.py
+++ vsproject/vsapp/views.py
@@ -1,9 +1,18 @@
 from django.shortcuts import render
 from rest_framework import viewsets
+from rest_framework import mixins
 from .serializers import viewsetserializer
 from .models import Student
 
 # Create your views here.
 class viewsetclass(viewsets.ReadOnlyModelViewSet):
+    serializer_class = viewsetserializer
+    queryset = Student.objects.all()
+
+#creating class which will provide all http methods and their responses
+class viewsetclass( mixins.RetrieveModelMixin,
+                    mixins.ListModelMixin,
+                    mixins.UpdateModelMixin,
+                    viewsets.GenericViewSet):
     serializer_class = viewsetserializer
     queryset = Student.objects.all()
